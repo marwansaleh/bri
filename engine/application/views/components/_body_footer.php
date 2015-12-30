7d6cd2d7b8bdba6a3cb9eee16f9904471dd6435c
marwansaleh <marwansaleh@192.168.1.101>
2015-12-30 17:32:11 +0700

    Add new master page

---
+++ engine/application/views/components/_body_footer.php
@@ -0,0 +1,50 @@
+<footer>
+    <div class="container">
+        <div class="content">
+            <div class="row">
+                <ul class="icon-list">
+                    <li>
+                        <figure class="icon-item">
+                            <img alt="logo-01" src="userfiles/logo/logo-01.jpg" class="img-responsive" />
+                        </figure>
+                    </li>
+                    <li>
+                        <figure class="icon-item">
+                            <img alt="logo-02" src="userfiles/logo/logo-02.jpg" class="img-responsive" />
+                        </figure>
+                    </li>
+                    <li>
+                        <figure class="icon-item">
+                            <img alt="logo-03" src="userfiles/logo/logo-03.jpg" class="img-responsive" />
+                        </figure>
+                    </li>
+                    <li>
+                        <figure class="icon-item">
+                            <img alt="logo-04" src="userfiles/logo/logo-04.jpg" class="img-responsive" />
+                        </figure>
+                    </li>
+                    <li>
+                        <figure class="icon-item">
+                            <img alt="logo-05" src="userfiles/logo/logo-05.jpg" class="img-responsive" />
+                        </figure>
+                    </li>
+                    <li>
+                        <figure class="icon-item">
+                            <img alt="logo-06" src="userfiles/logo/logo-06.jpg" class="img-responsive" />
+                        </figure>
+                    </li>
+                    <li>
+                        <figure class="icon-item">
+                            <img alt="logo-07" src="userfiles/logo/logo-07.jpg" class="img-responsive" />
+                        </figure>
+                    </li>
+                    <li>
+                        <figure class="icon-item">
+                            <img alt="logo-08" src="userfiles/logo/logo-08.jpg" class="img-responsive" />
+                        </figure>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
