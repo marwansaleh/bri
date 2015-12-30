52d81bca33f42c4eab1030a09f04021fe481d84d
marwansaleh <marwansaleh@192.168.1.101>
2015-12-30 17:32:55 +0700

    Add new master page

--- engine/application/views/components/_body_html.php
+++ engine/application/views/components/_body_html.php
@@ -5,6 +5,8 @@ if (isset($subview)){
     $this->load->view($subview);
 } 
 
+$this->load->view('components/_body_footer');
+
 /*
  * Filename: _body_html.php
  * Location: application/views/components/_body_html.php
