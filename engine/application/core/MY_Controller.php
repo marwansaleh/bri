a1e3410a8e60b8c36607c3c691d689a3efe16d1f
marwansaleh <marwansaleh@192.168.1.101>
2015-12-30 17:43:41 +0700

    add core controller

---
+++ engine/application/core/MY_Controller.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Description of MY_Controller
+ *
+ * @author marwansaleh 5:42:25 PM
+ */
+class MY_Controller {
+    //put your code here
+}
+
+/**
+ * Filename : MY_Controller.php
+ * Location : /MY_Controller.php
+ */
