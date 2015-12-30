1b65c4b06809d89ce21d1d822179b6447315b67a
marwansaleh <marwansaleh@192.168.1.101>
2015-12-30 17:10:36 +0700

    Add new helper: general helper

---
+++ engine/application/language/english/general_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['welcome_message'] = 'Welcome to website BRI';
+
+/*
+ * Filename: general_lang.php
+ * Location: application/language/english/general_lang.php
+ */
---
+++ engine/application/language/indonesia/general_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['welcome_message'] = 'Selamat datang di website BRI';
+
+/*
+ * Filename: general_lang.php
+ * Location: application/language/indonesia/general_lang.php
+ */
