66be71270b515cf18526c1b81410998421d5b45f
marwansaleh <marwansaleh@192.168.1.101>
2015-12-30 16:47:59 +0700

    Intall CI Framework

--- index.php
+++ index.php
@@ -1,59 +1,15 @@
 <?php
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
- * @link	http://codeigniter.com
- * @since	Version 1.0.0
- * @filesource
- */
+date_default_timezone_set('Asia/Jakarta');
 
-/*
- *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
- *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
- */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+//setting up ENVIRONMENT based on path location
+$dir = dirname(__FILE__);
+$production_path = '/production/path/here';
+$testing_path = '/testing/path/here';
+switch($dir){
+    case $production_path: define('ENVIRONMENT','production'); break;
+    case $testing_path: define('ENVIRONMENT','testing'); break;
+    default: define('ENVIRONMENT','development'); 
+}
 
 /*
  *---------------------------------------------------------------
@@ -63,30 +19,26 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-switch (ENVIRONMENT)
-{
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
-	break;
+switch (ENVIRONMENT) {
+    case 'development':
+        error_reporting(-1);
+        ini_set('display_errors', 1);
+        break;
 
-	case 'testing':
-	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-	break;
+    case 'testing':
+    case 'production':
+        ini_set('display_errors', 0);
+        if (version_compare(PHP_VERSION, '5.3', '>=')) {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+        } else {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+        }
+        break;
 
-	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
+    default:
+        header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+        echo 'The application environment is not set correctly.';
+        exit(1); // EXIT_ERROR
 }
 
 /*
@@ -98,7 +50,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = 'system';
+    $system_path = 'engine/system';
 
 /*
  *---------------------------------------------------------------
@@ -113,7 +65,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'application';
+    $application_folder = 'engine/application';
 
 /*
  *---------------------------------------------------------------
@@ -128,7 +80,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$view_folder = '';
+    $view_folder = '';
 
 
 /*
@@ -175,8 +127,24 @@ switch (ENVIRONMENT)
  *
  * Un-comment the $assign_to_config array below to use this feature
  */
-	// $assign_to_config['name_of_config_item'] = 'value of config item';
-
+    switch (ENVIRONMENT){
+        case 'production':
+            $assign_to_config['base_url'] = 'http://www.bri.co.id/';
+            $assign_to_config['cookie_domain'] = '.bri.co.id'; 
+            $assign_to_config['userfiles_base_url'] = 'http://www.bri.co.id/userfiles/';
+            $assign_to_config['userfiles_base_path'] = 'userfiles/';
+            $assign_to_config['assets_path'] = 'assets/';
+            $assign_to_config['lib_path'] = 'library/';
+            break;
+        case 'testing':
+            $assign_to_config['base_url'] = 'http://www.bri.co.id/'; break;
+        default:
+            $assign_to_config['base_url'] = 'http://localhost:8000/';
+            $assign_to_config['userfiles_base_url'] = 'http://localhost:8000/userfiles/';
+            $assign_to_config['userfiles_base_path'] = 'userfiles/';
+            $assign_to_config['assets_path'] = 'assets/';
+            $assign_to_config['lib_path'] = 'library/';
+    }
 
 
 // --------------------------------------------------------------------
@@ -189,98 +157,98 @@ switch (ENVIRONMENT)
  * ---------------------------------------------------------------
  */
 
-	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
+    // Set the current directory correctly for CLI requests
+    if (defined('STDIN'))
+    {
+        chdir(dirname(__FILE__));
+    }
 
-	if (($_temp = realpath($system_path)) !== FALSE)
-	{
-		$system_path = $_temp.'/';
-	}
-	else
-	{
-		// Ensure there's a trailing slash
-		$system_path = rtrim($system_path, '/').'/';
-	}
+    if (($_temp = realpath($system_path)) !== FALSE)
+    {
+        $system_path = $_temp.'/';
+    }
+    else
+    {
+        // Ensure there's a trailing slash
+        $system_path = rtrim($system_path, '/').'/';
+    }
 
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
-		exit(3); // EXIT_CONFIG
-	}
+    // Is the system path correct?
+    if ( ! is_dir($system_path))
+    {
+        header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+        echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
+        exit(3); // EXIT_CONFIG
+    }
 
 /*
  * -------------------------------------------------------------------
  *  Now that we know the path, set the main path constants
  * -------------------------------------------------------------------
  */
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+    // The name of THIS file
+    define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-	// Path to the system folder
-	define('BASEPATH', str_replace('\\', '/', $system_path));
+    // Path to the system folder
+    define('BASEPATH', str_replace('\\', '/', $system_path));
 
-	// Path to the front controller (this file)
-	define('FCPATH', dirname(__FILE__).'/');
+    // Path to the front controller (this file)
+    define('FCPATH', dirname(__FILE__).'/');
 
-	// Name of the "system folder"
-	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+    // Name of the "system folder"
+    define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
 
-	// The path to the "application" folder
-	if (is_dir($application_folder))
-	{
-		if (($_temp = realpath($application_folder)) !== FALSE)
-		{
-			$application_folder = $_temp;
-		}
+    // The path to the "application" folder
+    if (is_dir($application_folder))
+    {
+        if (($_temp = realpath($application_folder)) !== FALSE)
+        {
+            $application_folder = $_temp;
+        }
 
-		define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
-	}
-	else
-	{
-		if ( ! is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
-		{
-			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-			echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-			exit(3); // EXIT_CONFIG
-		}
+        define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
+    }
+    else
+    {
+        if ( ! is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
+        {
+            header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+            echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+            exit(3); // EXIT_CONFIG
+        }
 
-		define('APPPATH', BASEPATH.$application_folder.DIRECTORY_SEPARATOR);
-	}
+        define('APPPATH', BASEPATH.$application_folder.DIRECTORY_SEPARATOR);
+    }
 
-	// The path to the "views" folder
-	if ( ! is_dir($view_folder))
-	{
-		if ( ! empty($view_folder) && is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
-		{
-			$view_folder = APPPATH.$view_folder;
-		}
-		elseif ( ! is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
-		{
-			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-			echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-			exit(3); // EXIT_CONFIG
-		}
-		else
-		{
-			$view_folder = APPPATH.'views';
-		}
-	}
+    // The path to the "views" folder
+    if ( ! is_dir($view_folder))
+    {
+        if ( ! empty($view_folder) && is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
+        {
+            $view_folder = APPPATH.$view_folder;
+        }
+        elseif ( ! is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
+        {
+            header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+            echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+            exit(3); // EXIT_CONFIG
+        }
+        else
+        {
+            $view_folder = APPPATH.'views';
+        }
+    }
 
-	if (($_temp = realpath($view_folder)) !== FALSE)
-	{
-		$view_folder = $_temp.DIRECTORY_SEPARATOR;
-	}
-	else
-	{
-		$view_folder = rtrim($view_folder, '/\\').DIRECTORY_SEPARATOR;
-	}
+    if (($_temp = realpath($view_folder)) !== FALSE)
+    {
+        $view_folder = $_temp.DIRECTORY_SEPARATOR;
+    }
+    else
+    {
+        $view_folder = rtrim($view_folder, '/\\').DIRECTORY_SEPARATOR;
+    }
 
-	define('VIEWPATH', $view_folder);
+    define('VIEWPATH', $view_folder);
 
 /*
  * --------------------------------------------------------------------
