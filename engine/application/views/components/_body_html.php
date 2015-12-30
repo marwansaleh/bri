c71ffddb37a15c8755b3499b20cc0c606edbaff3
marwansaleh <marwansaleh@192.168.1.101>
2015-12-30 17:23:35 +0700

    Add new master page

--- engine/application/views/components/_body_html.php
+++ engine/application/views/components/_body_html.php
@@ -1,8 +1,9 @@
 <?php
+if (isset($subview)){
+    $this->load->view($subview);
+} 
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Filename: _body_html.php
+ * Location: application/views/components/_body_html.php
  */
-
