832bc4e31d2e0328b22f304bc6210f15ac5efa67
marwansaleh <marwansaleh@192.168.1.21>
2015-12-31 11:46:28 +0700

    add core controller

---
+++ engine/application/views/landingpage/index.php
@@ -0,0 +1,29 @@
+<div id="main">
+    <div class="container">
+        <div class="row">
+            <figure class="main-welcome-image">
+                <img src="<?php echo get_userfile('slides/image-01.jpg'); ?>" class="img-responsive" />
+            </figure>
+        </div>
+        <div class="row">
+            <div class="welcome-container">
+                <div class="content">
+                    <div class="col-xs-12 col-sm-6">
+                        <span class="welcome-title">SELAMAT DATANG DI WEBSITE BANK KEBANGGAAN INDONESIA</span>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <span>
+                            Bank Rakyat Indonesia (BRI) adalah salah satu bank milik pemerintah yang terbesar di 
+                            Indonesia. Pada awalnya Bank Rakyat Indonesia (BRI) didirikan di Purwokerto, Jawa 
+                            Tengah oleh Raden Bei Aria Wirjaatmadja dengan nama De Poerwokertosche Hulp en 
+                            Spaarbank der Inlandsche Hoofden atau "Bank Bantuan dan Simpanan Milik Kaum Priyayi 
+                            Purwokerto", suatu lembaga keuangan yang melayani orang-orang berkebangsaan 
+                            Indonesia (pribumi). Lembaga tersebut berdiri tanggal 16 Desember 1895, yang 
+                            kemudian dijadikan sebagai hari kelahiran BRI.
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
