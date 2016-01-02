b1c3486950f190eb77122e51f014971b9838762b
marwansaleh <marwansaleh@192.168.1.101>
2016-01-03 02:26:20 +0700

    Update header mobile

--- engine/application/migrations/004_add_menu.php
+++ engine/application/migrations/004_add_menu.php
@@ -24,17 +24,38 @@ class Migration_add_menu extends CI_Migration {
                 'constraint' => 11,
                 'unsigned' => TRUE
             ),
-            'caption' => array(
+            'caption_id' => array(
                 'type'  => 'VARCHAR',
                 'constraint' => 30
             ),
-            'title' => array(
+            'caption_en' => array(
+                'type'  => 'VARCHAR',
+                'constraint' => 30
+            ),
+            'title_id' => array(
                 'type' => 'VARCHAR',
-                'constraint' => 150
+                'constraint' => 150,
+                'null' => TRUE
+            ),
+            'title_en' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 150,
+                'null' => TRUE
             ),
             'href'   => array(
                 'type'  => 'VARCHAR',
-                'constraint' => 254
+                'constraint' => 254,
+                'null' => TRUE
+            ),
+            'category'   => array(
+                'type'  => 'TINYINT',
+                'constraint' => 1,
+                'default' => CT_MAINMENU_CORPORATE
+            ),
+            'sort'   => array(
+                'type'  => 'iNT',
+                'constraint' => 9,
+                'default' => 0
             )
         ));
         
@@ -46,11 +67,170 @@ class Migration_add_menu extends CI_Migration {
         }
         //build the table if not exists (set second param TRUE)
         $this->dbforge->create_table($this->_table_name, TRUE, $this->_attributes);
+        
+        //Need seeding ?
+        $this->_seed(array(
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Situs Perusahaan',
+                'caption_en'    => 'Corporate Website',
+                'title_id'      => 'Situs Perusahaan',
+                'title_en'      => 'Corporate website',
+                'href'          => 'corporate',
+                'category'      => CT_MAINMENU_HOME,
+                'sort'          => 0,
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Karir',
+                'caption_en'    => 'Career',
+                'title_id'      => 'Karir',
+                'title_en'      => 'Career',
+                'href'          => 'http://karir.bri.co.id',
+                'category'      => CT_MAINMENU_HOME,
+                'sort'          => 1
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Hubungan Investor',
+                'caption_en'    => 'Investor Relationship',
+                'title_id'      => 'Hubungan investor',
+                'title_en'      => 'Investor relationship',
+                'href'          => 'http://ib.bri.co.id',
+                'category'      => CT_MAINMENU_HOME,
+                'sort'          => 2
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Anak Perusahaan',
+                'caption_en'    => 'Subsidiary',
+                'title_id'      => 'Anak perusahaan',
+                'title_en'      => 'Subsidiary',
+                'href'          => 'http://karir.bri.co.id',
+                'category'      => CT_MAINMENU_HOME,
+                'sort'          => 3
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'CSR BRI',
+                'caption_en'    => 'CSR BRI',
+                'title_id'      => 'CSR BRI',
+                'title_en'      => 'CSR RI',
+                'href'          => 'http://karir.bri.co.id',
+                'category'      => CT_MAINMENU_HOME,
+                'sort'          => 4
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Tentang Kami',
+                'caption_en'    => 'About Us',
+                'title_id'      => 'Tentang kami',
+                'title_en'      => 'About us',
+                'href'          => 'corporate/aboutus',
+                'category'      => CT_MAINMENU_CORPORATE,
+                'sort'          => 0
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Simpanan',
+                'caption_en'    => 'Savings',
+                'title_id'      => 'Simpanan',
+                'title_en'      => 'Savings',
+                'href'          => 'corporate/saving',
+                'category'      => CT_MAINMENU_CORPORATE,
+                'sort'          => 1
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Pinjaman',
+                'caption_en'    => 'Loans',
+                'title_id'      => 'Pinjaman',
+                'title_en'      => 'Loans',
+                'href'          => 'corporate/loans',
+                'category'      => CT_MAINMENU_CORPORATE,
+                'sort'          => 2
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Internasional',
+                'caption_en'    => 'International',
+                'title_id'      => 'Internasional',
+                'title_en'      => 'International',
+                'href'          => 'corporate/internasional',
+                'category'      => CT_MAINMENU_CORPORATE,
+                'sort'          => 3
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Jasa Bank',
+                'caption_en'    => 'Bank Services',
+                'title_id'      => 'Jasa Bank',
+                'title_en'      => 'Bank Services',
+                'href'          => 'corporate/bankservice',
+                'category'      => CT_MAINMENU_CORPORATE,
+                'sort'          => 4
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Produk Konsumer',
+                'caption_en'    => 'Consumer Banking',
+                'title_id'      => 'Produk Konsumer',
+                'title_en'      => 'Consumer Banking',
+                'href'          => 'corporate/consumerbanking',
+                'category'      => CT_MAINMENU_CORPORATE,
+                'sort'          => 5
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Investasi Perbankan',
+                'caption_en'    => 'Investment Banking',
+                'title_id'      => 'Investasi Perbankan',
+                'title_en'      => 'Investment Banking',
+                'href'          => 'corporate/investmentbanking',
+                'category'      => CT_MAINMENU_CORPORATE,
+                'sort'          => 6
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Layanan Prioritas',
+                'caption_en'    => 'Priority Banking',
+                'title_id'      => 'Layanan Prioritas',
+                'title_en'      => 'Priority Banking',
+                'href'          => 'corporate/prioritybanking',
+                'category'      => CT_MAINMENU_CORPORATE,
+                'sort'          => 7
+            ),
+            array(
+                'parent_id'     => 0,
+                'caption_id'    => 'Info Lelang',
+                'caption_en'    => 'Auction Information',
+                'title_id'      => 'Info Lelang',
+                'title_en'      => 'Auction Information',
+                'href'          => 'corporate/auctioninfo',
+                'category'      => CT_MAINMENU_CORPORATE,
+                'sort'          => 8
+            )
+        ));
     }
     
     public function down(){
         //remove the table if exists
         $this->dbforge->drop_table($this->_table_name, TRUE);
+    }
+    
+    private function _seed($array){
+        $count = 0;
+        if (!isset($this->db)){
+            $this->load->database();
+        }
+        
+        if (is_array($array)){
+            for ($i=0; $i<count($array); $i++){
+                $this->db->insert($this->_table_name, $array[$i]);
+                $count++;
+            }
+        }
+        return $count;
     }
 }
 
