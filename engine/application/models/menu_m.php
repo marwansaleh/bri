8d9ac27e128e1a6fcdef34332e33cfa243493414
marwansaleh <marwansaleh@192.168.1.101>
2016-01-03 02:52:01 +0700

    Core model class

---
+++ engine/application/models/menu_m.php
@@ -0,0 +1,70 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Description of Menu_m
+ *
+ * @author Marwan
+ * @email amazzura.biz@gmail.com
+ */
+class Menu_m extends MY_Model {
+    protected $_table_name = 'menu';
+    protected $_primary_key = 'id';
+    protected $_primary_filter = 'intval';
+    protected $_order_by = 'sort, caption';
+    protected $_timestamps = TRUE;
+    protected $_timestamps_field = array('created','modified');
+    
+    public $rules = array(
+        'parent_id' => array(
+            'field' => 'parent_id',
+            'label' => 'Parent ID', 
+            'rules' => 'required|trim|xss_clean'
+        ),
+        'caption_id' => array(
+            'field' => 'caption_id',
+            'label' => 'Caption in Bahasa', 
+            'rules' => 'trim|xss_clean'
+        ),
+        'caption_en' => array(
+            'field' => 'caption_en',
+            'label' => 'Caption in English', 
+            'rules' => 'trim|xss_clean'
+        ),
+        'title_id' => array(
+            'field' => 'title_id',
+            'label' => 'Title in Bahasa', 
+            'rules' => 'trim|xss_clean'
+        ),
+        'title_en' => array(
+            'field' => 'title_en',
+            'label' => 'Title in English', 
+            'rules' => 'trim|xss_clean'
+        ),
+        'href' => array(
+            'field' => 'href',
+            'label' => 'URL link', 
+            'rules' => 'trim|xss_clean'
+        ),
+        'category' => array(
+            'field' => 'category',
+            'label' => 'Menu category', 
+            'rules' => 'trim|xss_clean'
+        ),
+        'sort' => array(
+            'field' => 'sort',
+            'label' => 'Display index order', 
+            'rules' => 'trim|xss_clean'
+        )
+    );
+    
+    public function save($data, $id = NULL) {
+        if (!$id && !isset($data['inserted'])){
+            $data['inserted'] = time();
+        }
+        return parent::save($data, $id);
+    }
+}
+
+/*
+ * file location: /application/models/menu_m.php
+ */
